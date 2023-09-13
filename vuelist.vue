e11c6f42d95f449ff96aa11f140784e3beac1d27
Mariana Granados <mgranados09@alumnos.uaq.mx>
2023-09-12 22:46:34 -0600

    update comments for comprehensiveness

--- vuelist.vue
+++ vuelist.vue
@@ -12,12 +12,22 @@ const todos = ref([
 ])
 
 function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value })
+  // ...
+  todos.value.push({id: id++, text: newTodo.value})
+  //adds new todo value from v-model="newTodo" input
   newTodo.value = ''
 }
 
 function removeTodo(todo) {
-  todos.value = todos.value.filter((t) => t !== todo)
+  // remove a specific todo item from the todos array
+  //function takes one parameter, 'todo'. This parameter represents
+  //the todo item that you want to remove from the todos array
+  
+  //the filter method is used to create a new array that includes only
+  //the elements that meet a specific condition, in this case, it's used to
+  // create a new array with all the todo items except the one that matches
+  //the todo parameter 
+  todos.value = todos.value.filter((t) => t != todo)
 }
 </script>
 
@@ -29,6 +39,7 @@ function removeTodo(todo) {
   <ul>
     <li v-for="todo in todos" :key="todo.id">
       {{ todo.text }}
+      <!--The click directive binds the removeTodo function to the buttons click event and passes the todo object associated with the current iteration of the v-loop as a parameter -->
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>

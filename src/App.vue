bed3aa41313bb2d4e760854c5c0cd8d53003ef59
Mariana Granados <mgranados09@alumnos.uaq.mx>
2023-09-12 23:10:46 -0600

    1st task: simple user interface using vue.js

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const heading = ref('Compute Product')
+const compute = ref(true)
+
+let num1 = 5
+let num2 = 3
+
+function comp(){
+  compute.value = !compute.value
+}
+
+</script>
+
+<template>
+  <h1>{{heading}}</h1>
+  <img src="https://shorturl.at/uxJR3" alt="Electronics!" height="500">
+  <button v-if="compute" @click="comp" style="display: block; padding:2%; margin-top: 2%;">Compute!</button>
+  <p v-else>The product is: {{num1*num2}}</p>
+</template>
